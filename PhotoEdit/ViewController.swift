35874a9e471fc975c20f45614e384170d9da43b6
20029981 <20029981@it103imac20.wit.ie>
2017-03-30 15:23:46 +0100

    Initial Commit

---
+++ PhotoEdit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoEdit
+//
+//  Created by 20029981 on 30/03/2017.
+//  Copyright © 2017 20029981. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
