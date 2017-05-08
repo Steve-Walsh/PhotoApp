0c7025c3da2ba6a15f54fb7e63191322afbbc103
Stephen Walsh <steve@stephens-mac.local>
2017-05-08 14:26:19 +0100

    Added cancel button, added new functions for show and hide of ui elements, and added the saving of newly edited images

--- PhotoEdit/ViewController.swift
+++ PhotoEdit/ViewController.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2017 20029981. All rights reserved.
 //
 
-import UIKit
+import UIKit;
+//import HRToast;
+//let themeColor = UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0);
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
@@ -17,7 +19,10 @@ UINavigationControllerDelegate {
     @IBOutlet weak var colorButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    var viewForScreenShot: UIView!
     
+    
+    @IBOutlet weak var cancelButton: UIButton!
     var lastPoint = CGPoint.zero
     var red: CGFloat = 0.0
     var green: CGFloat = 0.0
@@ -26,6 +31,7 @@ UINavigationControllerDelegate {
     var opacity: CGFloat = 1.0
     var swiped = false
     var gotImage = false;
+    
     
     @IBOutlet weak var buttonView: UIStackView!
     
@@ -36,13 +42,13 @@ UINavigationControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorButton.isEnabled = false;
-        saveButton.isEnabled = false;
-        colorButton.alpha = 0;
-        saveButton.alpha = 0;
+
+        hideImageStuff()
+
         // Do any additional setup after loading the view, typically from a nib.
         
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -75,14 +81,7 @@ UINavigationControllerDelegate {
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        buttonView.isHidden = true;
-        takePictureButton.isEnabled = false;
-        galleryButton.isEnabled = false;
-        gotImage = true;
-        saveButton.isEnabled = true;
-        saveButton.alpha = 1;
-        colorButton.isEnabled = true;
-        colorButton.alpha = 1;
+        showImageStuff()
         imageView.image = image
         self.dismiss(animated: true, completion: nil);
     }
@@ -128,14 +127,81 @@ UINavigationControllerDelegate {
             UIGraphicsEndImageContext()
         }
     }
-
     
-    
+    @IBAction func saveImage(_ sender: Any) {
+        _ = imageView.screenShot;
+        hideImageStuff()
+        
+         self.view.makeToast(message: "Picture Saved", duration: 2.0, position:HRToastPositionCenter as AnyObject)
 
-
+    }
+    @IBAction func cancel(_ sender: Any) {
+        hideImageStuff()
+    }
     
+    func hideImageStuff(){
+        buttonView.isHidden = false;
+        takePictureButton.isEnabled = true;
+        galleryButton.isEnabled = true;
+        gotImage = false;
+        colorButton.isEnabled = false;
+        saveButton.isEnabled = false;
+        colorButton.alpha = 0;
+        saveButton.alpha = 0;
+        cancelButton.isEnabled = false;
+        cancelButton.alpha = 0;
+        imageView.isHidden = true;
+        
+    }
+    func showImageStuff(){
+        buttonView.isHidden = true;
+        takePictureButton.isEnabled = false;
+        galleryButton.isEnabled = false;
+        gotImage = true;
+        saveButton.isEnabled = true;
+        saveButton.alpha = 1;
+        colorButton.isEnabled = true;
+        colorButton.alpha = 1;
+        cancelButton.isEnabled = true;
+        cancelButton.alpha = 1;
+        imageView.isHidden = false;
+    }
     
         
 
 }
+extension UIImageView {
+    
+    var screenShot: UIImageView?  {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, false, 1.0);
+        if let _ = UIGraphicsGetCurrentContext() {
+            drawHierarchy(in: bounds, afterScreenUpdates: true)
+            let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            UIImageWriteToSavedPhotosAlbum((screenshot)!, nil, nil, nil)
+//            return true
+        }
+        return nil
+    }
+}
+extension UIViewController {
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+//        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    } }
 
